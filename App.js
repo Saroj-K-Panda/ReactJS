9d1b91cda003f29e7699bd02df38937c571596c5
Saroj Panda <saroj.microsoft@gmail.com>
2025-07-06 23:21:42 +0530

    code checking

--- App.js
+++ App.js
@@ -1,3 +1,22 @@
-const headingSP = React.createElement("h2",{},"3rd July is the birthday for me !!!")
-        const root = ReactDOM.createRoot(document.getElementById("root"))
-        root.render(headingSP)
+
+const parent = React.createElement("div", {id:"parent"},[
+    React.createElement("div",{id:"child1", key: "child1"},
+        React.createElement("h1", {},"I am at h1 tag"),
+        React.createElement("h2",{}, "I am at h2 tag"), 
+    ),
+      React.createElement("div",{id:"child2", key: "child2"},
+        React.createElement("h1", {},"I am at h1 tag"),
+        React.createElement("h2",{}, "I am at h2 tag"), 
+    )
+]);
+
+
+
+
+const headingSP = React.createElement(
+  "h2",
+  { id: "heading", xyz: "abc" },
+  "3rd July is the birthday for me !!!"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(headingSP);
