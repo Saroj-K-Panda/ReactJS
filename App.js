293ca0269be1388dcd3da405f015bfc6fc462da6
Saroj Panda <saroj.microsoft@gmail.com>
2025-07-06 10:05:49 +0530

    changes commit

---
+++ App.js
@@ -0,0 +1,3 @@
+const headingSP = React.createElement("h2",{},"3rd July is the birthday for me !!!")
+        const root = ReactDOM.createRoot(document.getElementById("root"))
+        root.render(headingSP)
